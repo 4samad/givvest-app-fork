de0920dbeff06bf7496e599a77f02fdef6f6c665
4samad <smd2k15@gmail.com>
2024-07-24 22:15:09 +0400

    :adhesive_bandage: make counter public :art: format code

--- packages/hardhat/contracts/Givvest.sol
+++ packages/hardhat/contracts/Givvest.sol
@@ -8,28 +8,28 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IGivvestCause {
-    /**
-     * @notice Raise a specific amount for a cause identified by the tokenId.
-     * @param tokenId The ID of the cause token.
-     * @param amount The amount to raise for the cause.
-     */
-    function raiseAmount(uint256 tokenId, uint256 amount) external;
+	/**
+	 * @notice Raise a specific amount for a cause identified by the tokenId.
+	 * @param tokenId The ID of the cause token.
+	 * @param amount The amount to raise for the cause.
+	 */
+	function raiseAmount(uint256 tokenId, uint256 amount) external;
 
-    /**
-     * @notice Get the address of the fundraiser associated with a cause token.
-     * @param tokenId The ID of the cause token.
-     * @return The address of the fundraiser.
-     */
-    function getFundraiser(uint256 tokenId) external view returns (address);
+	/**
+	 * @notice Get the address of the fundraiser associated with a cause token.
+	 * @param tokenId The ID of the cause token.
+	 * @return The address of the fundraiser.
+	 */
+	function getFundraiser(uint256 tokenId) external view returns (address);
 }
 
 interface IGivvestCoin {
-    /**
-     * @notice Mint Givvest coins to a specified address.
-     * @param to The address to receive the minted coins.
-     * @param amount The amount of coins to mint.
-     */
-    function mint(address to, uint256 amount) external;
+	/**
+	 * @notice Mint Givvest coins to a specified address.
+	 * @param to The address to receive the minted coins.
+	 * @param amount The amount of coins to mint.
+	 */
+	function mint(address to, uint256 amount) external;
 }
 
 /**
@@ -37,164 +37,192 @@ interface IGivvestCoin {
  * @dev Main contract for the Givvest platform, which allows users to donate to causes and receive NFT representations of their donations.
  */
 contract Givvest is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+	IGivvestCause public givvestCauseContract;
+	IGivvestCoin public givvestCoinContract;
 
-    IGivvestCause public givvestCauseContract;
-    IGivvestCoin public givvestCoinContract;
+	struct DonationNFT {
+		uint256 amount;
+		uint256 causeId;
+		bool listedForRedemption;
+	}
 
-    struct DonationNFT {
-        uint256 amount;
-        uint256 causeId;
-        bool listedForRedemption;
-    }
+	uint256 public tokenCount;
 
-    uint256 private _nextTokenId;
+	// Mapping from token ID to DonationNFT
+	mapping(uint256 => DonationNFT) public donationNFTs;
 
-    // Mapping from token ID to DonationNFT
-    mapping(uint256 => DonationNFT) public donationNFTs;
+	// Mapping from token ID to permanence status
+	mapping(uint256 => bool) public isPermanent;
 
-    // Mapping from token ID to permanence status
-    mapping(uint256 => bool) public isPermanent;
+	// Events for logging important actions
+	event DonationMinted(
+		address indexed donor,
+		uint256 tokenId,
+		uint256 amount,
+		uint256 causeId
+	);
+	event DonationPermanent(
+		address indexed donor,
+		uint256 tokenId,
+		uint256 amount
+	);
+	event DonationListed(address indexed donor, uint256 tokenId);
 
-    // Events for logging important actions
-    event DonationMinted(address indexed donor, uint256 tokenId, uint256 amount, uint256 causeId);
-    event DonationPermanent(address indexed donor, uint256 tokenId, uint256 amount);
-    event DonationListed(address indexed donor, uint256 tokenId);
+	modifier onlyDonor(uint256 tokenId) {
+		require(msg.sender == ownerOf(tokenId), "Not the donor");
+		_;
+	}
 
-    modifier onlyDonor(uint256 tokenId) {
-        require(msg.sender == ownerOf(tokenId), "Not the donor");
-        _;
-    }
+	modifier notPermanentNFT(uint256 tokenId) {
+		require(!isPermanent[tokenId], "It is a permanent Donation");
+		_;
+	}
 
-    modifier notPermanentNFT(uint256 tokenId) {
-        require(!isPermanent[tokenId], "It is a permanent Donation");
-        _;
-    }
+	modifier notZeroValue() {
+		require(msg.value > 0, "Donation must be greater than zero");
+		_;
+	}
 
-    modifier notZeroValue() {
-        require(msg.value > 0, "Donation must be greater than zero");
-        _;
-    }
+	/**
+	 * @dev Initializes the contract with the given addresses.
+	 * @param initialOwner The address of the initial owner.
+	 * @param givvestCoinAddress The address of the GivvestCoin contract.
+	 * @param givvestCauseAddress The address of the GivvestCause contract.
+	 */
+	constructor(
+		address initialOwner,
+		address givvestCoinAddress,
+		address givvestCauseAddress
+	) ERC721("GivvestNFT", "GVNFT") Ownable(initialOwner) {
+		givvestCauseContract = IGivvestCause(givvestCauseAddress);
+		givvestCoinContract = IGivvestCoin(givvestCoinAddress);
+	}
 
-    /**
-     * @dev Initializes the contract with the given addresses.
-     * @param initialOwner The address of the initial owner.
-     * @param givvestCoinAddress The address of the GivvestCoin contract.
-     * @param givvestCauseAddress The address of the GivvestCause contract.
-     */
-    constructor(address initialOwner, address givvestCoinAddress, address givvestCauseAddress)
-        ERC721("GivvestNFT", "GVNFT")
-        Ownable(initialOwner)
-    {
-        givvestCauseContract = IGivvestCause(givvestCauseAddress);
-        givvestCoinContract = IGivvestCoin(givvestCoinAddress);
-    }
+	/**
+	 * @dev Internal function to mint a new DonationNFT.
+	 * @param causeId The ID of the cause associated with the donation.
+	 */
+	function _mintDonationNFT(uint256 causeId) internal {
+		uint256 tokenId = tokenCount++;
+		_safeMint(msg.sender, tokenId);
 
-    /**
-     * @dev Internal function to mint a new DonationNFT.
-     * @param causeId The ID of the cause associated with the donation.
-     */
-    function _mintDonationNFT(uint256 causeId) internal {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
+		donationNFTs[tokenId] = DonationNFT({
+			amount: msg.value,
+			causeId: causeId,
+			listedForRedemption: false
+		});
 
-        donationNFTs[tokenId] = DonationNFT({
-            amount: msg.value,
-            causeId: causeId,
-            listedForRedemption: false
-        });
+		emit DonationMinted(msg.sender, tokenId, msg.value, causeId);
+	}
 
-        emit DonationMinted(msg.sender, tokenId, msg.value, causeId);
-    }
+	/**
+	 * @notice Donate to a specific cause and receive a DonationNFT.
+	 * @param causeId The ID of the cause to donate to.
+	 */
+	function donateToCause(uint256 causeId) public payable notZeroValue {
+		// Check
+		try givvestCauseContract.raiseAmount(causeId, msg.value) {
+			// Effects
+			_mintDonationNFT(causeId);
 
-    /**
-     * @notice Donate to a specific cause and receive a DonationNFT.
-     * @param causeId The ID of the cause to donate to.
-     */
-    function donateToCause(uint256 causeId) public payable notZeroValue {
-        // Check
-        try givvestCauseContract.raiseAmount(causeId, msg.value) {
-            // Effects
-            _mintDonationNFT(causeId);
-            
-            // Interactions
-            address payable fundraiser = payable(givvestCauseContract.getFundraiser(causeId));
-            (bool success, ) = fundraiser.call{value: msg.value}("");
-            require(success, "Fund transfer failed");
-        } catch Error(string memory reason) {
-            revert(reason);
-        } catch (bytes memory lowLevelData) {
-            revert(string(abi.encodePacked("An unknown error occurred: ", lowLevelData)));
-        }
-    }
+			// Interactions
+			address payable fundraiser = payable(
+				givvestCauseContract.getFundraiser(causeId)
+			);
+			(bool success, ) = fundraiser.call{ value: msg.value }("");
+			require(success, "Fund transfer failed");
+		} catch Error(string memory reason) {
+			revert(reason);
+		} catch (bytes memory lowLevelData) {
+			revert(
+				string(
+					abi.encodePacked(
+						"An unknown error occurred: ",
+						lowLevelData
+					)
+				)
+			);
+		}
+	}
 
-    /**
-     * @notice Donate to an existing DonationNFT.
-     * @param tokenId The ID of the DonationNFT to donate to.
-     */
-    function donateToDonationNFT(uint256 tokenId) public payable notZeroValue notPermanentNFT(tokenId) {
-        // Check
-        require(msg.value <= donationNFTs[tokenId].amount, "Donation amount is more than needed");
-        
-        // Effects
-        donationNFTs[tokenId].amount -= msg.value;
-        _mintDonationNFT(donationNFTs[tokenId].causeId);
+	/**
+	 * @notice Donate to an existing DonationNFT.
+	 * @param tokenId The ID of the DonationNFT to donate to.
+	 */
+	function donateToDonationNFT(
+		uint256 tokenId
+	) public payable notZeroValue notPermanentNFT(tokenId) {
+		// Check
+		require(
+			msg.value <= donationNFTs[tokenId].amount,
+			"Donation amount is more than needed"
+		);
 
-        // Interactions
-        address payable originalDonor = payable(ownerOf(tokenId));
-        (bool success, ) = originalDonor.call{value: msg.value}("");
-        require(success, "Transfer to donor failed");
-    }
+		// Effects
+		donationNFTs[tokenId].amount -= msg.value;
+		_mintDonationNFT(donationNFTs[tokenId].causeId);
 
-    /**
-     * @notice List a DonationNFT for redemption.
-     * @param tokenId The ID of the DonationNFT to list for redemption.
-     */
-    function listForRedemption(uint256 tokenId) public onlyDonor(tokenId) notPermanentNFT(tokenId) {
-        donationNFTs[tokenId].listedForRedemption = true;
-        emit DonationListed(msg.sender, tokenId);
-    }
+		// Interactions
+		address payable originalDonor = payable(ownerOf(tokenId));
+		(bool success, ) = originalDonor.call{ value: msg.value }("");
+		require(success, "Transfer to donor failed");
+	}
 
-    /**
-     * @notice Make a DonationNFT permanent.
-     * @param tokenId The ID of the DonationNFT to make permanent.
-     */
-    function makeDonationPermanent(uint256 tokenId) public onlyDonor(tokenId) notPermanentNFT(tokenId) {
-        donationNFTs[tokenId].listedForRedemption = false;
-        isPermanent[tokenId] = true;
-        givvestCoinContract.mint(msg.sender, donationNFTs[tokenId].amount);
-        emit DonationPermanent(msg.sender, tokenId, donationNFTs[tokenId].amount);
-    }
+	/**
+	 * @notice List a DonationNFT for redemption.
+	 * @param tokenId The ID of the DonationNFT to list for redemption.
+	 */
+	function listForRedemption(
+		uint256 tokenId
+	) public onlyDonor(tokenId) notPermanentNFT(tokenId) {
+		donationNFTs[tokenId].listedForRedemption = true;
+		emit DonationListed(msg.sender, tokenId);
+	}
 
-    /**
-     * @dev Override for `_update` function to ensure compatibility with multiple inheritance.
-     */
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
+	/**
+	 * @notice Make a DonationNFT permanent.
+	 * @param tokenId The ID of the DonationNFT to make permanent.
+	 */
+	function makeDonationPermanent(
+		uint256 tokenId
+	) public onlyDonor(tokenId) notPermanentNFT(tokenId) {
+		donationNFTs[tokenId].listedForRedemption = false;
+		isPermanent[tokenId] = true;
+		givvestCoinContract.mint(msg.sender, donationNFTs[tokenId].amount);
+		emit DonationPermanent(
+			msg.sender,
+			tokenId,
+			donationNFTs[tokenId].amount
+		);
+	}
 
-    /**
-     * @dev Override for `_increaseBalance` function to ensure compatibility with multiple inheritance.
-     */
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
+	/**
+	 * @dev Override for `_update` function to ensure compatibility with multiple inheritance.
+	 */
+	function _update(
+		address to,
+		uint256 tokenId,
+		address auth
+	) internal override(ERC721, ERC721Enumerable) returns (address) {
+		return super._update(to, tokenId, auth);
+	}
 
-    /**
-     * @dev Override for `supportsInterface` function to ensure compatibility with multiple inheritance.
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+	/**
+	 * @dev Override for `_increaseBalance` function to ensure compatibility with multiple inheritance.
+	 */
+	function _increaseBalance(
+		address account,
+		uint128 value
+	) internal override(ERC721, ERC721Enumerable) {
+		super._increaseBalance(account, value);
+	}
+
+	/**
+	 * @dev Override for `supportsInterface` function to ensure compatibility with multiple inheritance.
+	 */
+	function supportsInterface(
+		bytes4 interfaceId
+	) public view override(ERC721, ERC721Enumerable) returns (bool) {
+		return super.supportsInterface(interfaceId);
+	}
 }
--- packages/hardhat/contracts/GivvestCause.sol
+++ packages/hardhat/contracts/GivvestCause.sol
@@ -12,171 +12,200 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
  * @title GivvestCause
  * @dev Contract for managing fundraising causes using NFTs.
  */
-contract GivvestCause is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl {
-    struct Cause {
-        address fundraiser;
-        uint256 amountNeeded;
-        uint256 amountRaised;
-        bool isActive;
-    }
+contract GivvestCause is
+	ERC721,
+	ERC721Enumerable,
+	ERC721URIStorage,
+	ERC721Burnable,
+	AccessControl
+{
+	struct Cause {
+		address fundraiser;
+		uint256 amountNeeded;
+		uint256 amountRaised;
+		bool isActive;
+	}
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant GIVVEST_ROLE = keccak256("GIVVEST_ROLE");
-    uint256 private _nextTokenId;
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+	bytes32 public constant GIVVEST_ROLE = keccak256("GIVVEST_ROLE");
+	uint256 public tokenCount;
 
-    // Mapping from token ID to Cause details
-    mapping(uint256 => Cause) public causes;
+	// Mapping from token ID to Cause details
+	mapping(uint256 => Cause) public causes;
 
-    // Events for logging important actions
-    event CauseMinted(uint256 tokenId, address fundraiser, uint256 amountNeeded);
-    event AmountRaised(uint256 tokenId, uint256 amount);
-    event CauseStateToggled(uint256 tokenId, bool isActive);
-    event OwnershipChanged(address newOwner);
+	// Events for logging important actions
+	event CauseMinted(
+		uint256 tokenId,
+		address fundraiser,
+		uint256 amountNeeded
+	);
+	event AmountRaised(uint256 tokenId, uint256 amount);
+	event CauseStateToggled(uint256 tokenId, bool isActive);
+	event OwnershipChanged(address newOwner);
 
-    /**
-     * @dev Initializes the contract, setting up roles.
-     * @param initialOwner The address of the initial owner.
-     */
-    constructor(address initialOwner)
-        ERC721("GivvestCause", "GCOS")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
-        _grantRole(MINTER_ROLE, initialOwner);
-    }
+	/**
+	 * @dev Initializes the contract, setting up roles.
+	 * @param initialOwner The address of the initial owner.
+	 */
+	constructor(address initialOwner) ERC721("GivvestCause", "GCOS") {
+		_grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+		_grantRole(MINTER_ROLE, initialOwner);
+	}
 
-    /**
-     * @notice Safely mint a new token with cause details.
-     * @param uri The URI of the token metadata.
-     * @param fundraiser The address of the fundraiser.
-     * @param amountNeeded The amount needed for the cause.
-     */
-    function safeMint(string memory uri, address fundraiser, uint256 amountNeeded) public onlyRole(MINTER_ROLE) {
-        require(amountNeeded > 0, "Amount needed must be greater than zero");
-        require(fundraiser != address(0), "Invalid fundraiser address");
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, uri);
+	/**
+	 * @notice Safely mint a new token with cause details.
+	 * @param uri The URI of the token metadata.
+	 * @param fundraiser The address of the fundraiser.
+	 * @param amountNeeded The amount needed for the cause.
+	 */
+	function safeMint(
+		string memory uri,
+		address fundraiser,
+		uint256 amountNeeded
+	) public onlyRole(MINTER_ROLE) {
+		require(amountNeeded > 0, "Amount needed must be greater than zero");
+		require(fundraiser != address(0), "Invalid fundraiser address");
+		//assigns tokenCount to tokenId before incrementing
+		uint256 tokenId = tokenCount++;
+		_safeMint(msg.sender, tokenId);
+		_setTokenURI(tokenId, uri);
 
-        causes[tokenId] = Cause({
-            fundraiser: fundraiser,
-            amountNeeded: amountNeeded,
-            amountRaised: 0,
-            isActive: true
-        });
+		causes[tokenId] = Cause({
+			fundraiser: fundraiser,
+			amountNeeded: amountNeeded,
+			amountRaised: 0,
+			isActive: true
+		});
 
-        emit CauseMinted(tokenId, fundraiser, amountNeeded);
-    }
+		emit CauseMinted(tokenId, fundraiser, amountNeeded);
+	}
 
-    /**
-     * @notice Raise an amount for a specific cause.
-     * @param tokenId The ID of the cause token.
-     * @param amount The amount to raise.
-     */
-    function raiseAmount(uint256 tokenId, uint256 amount) public onlyRole(GIVVEST_ROLE) {
-        Cause storage cause = causes[tokenId];
-        require(cause.isActive, "Cause is not actively fundraising");
-        require(cause.amountRaised + amount <= cause.amountNeeded, "Amount is more than needed");
-        cause.amountRaised += amount;
-        if (cause.amountRaised == cause.amountNeeded) {
-            cause.isActive = false;
-        }
+	/**
+	 * @notice Raise an amount for a specific cause.
+	 * @param tokenId The ID of the cause token.
+	 * @param amount The amount to raise.
+	 */
+	function raiseAmount(
+		uint256 tokenId,
+		uint256 amount
+	) public onlyRole(GIVVEST_ROLE) {
+		Cause storage cause = causes[tokenId];
+		require(cause.isActive, "Cause is not actively fundraising");
+		require(
+			cause.amountRaised + amount <= cause.amountNeeded,
+			"Amount is more than needed"
+		);
+		cause.amountRaised += amount;
+		if (cause.amountRaised == cause.amountNeeded) {
+			cause.isActive = false;
+		}
 
-        emit AmountRaised(tokenId, amount);
-    }
+		emit AmountRaised(tokenId, amount);
+	}
 
-    /**
-     * @notice Toggle the active state of a cause.
-     * @param tokenId The ID of the cause token.
-     */
-    function toggleCauseState(uint256 tokenId) public {
-        Cause storage cause = causes[tokenId];
-        require(cause.isActive || cause.amountRaised < cause.amountNeeded, "Fundraising is already complete");
-        require(cause.fundraiser == msg.sender || hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Not permitted");
-        cause.isActive = !cause.isActive;
+	/**
+	 * @notice Toggle the active state of a cause.
+	 * @param tokenId The ID of the cause token.
+	 */
+	function toggleCauseState(uint256 tokenId) public {
+		Cause storage cause = causes[tokenId];
+		require(
+			cause.isActive || cause.amountRaised < cause.amountNeeded,
+			"Fundraising is already complete"
+		);
+		require(
+			cause.fundraiser == msg.sender ||
+				hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+			"Not permitted"
+		);
+		cause.isActive = !cause.isActive;
 
-        emit CauseStateToggled(tokenId, cause.isActive);
-    }
+		emit CauseStateToggled(tokenId, cause.isActive);
+	}
 
-    /**
-     * @notice Get the address of the fundraiser for a specific cause.
-     * @param tokenId The ID of the cause token.
-     * @return The address of the fundraiser.
-     */
-    function getFundraiser(uint256 tokenId) public view returns (address) {
-        return causes[tokenId].fundraiser;
-    }
+	/**
+	 * @notice Get the address of the fundraiser for a specific cause.
+	 * @param tokenId The ID of the cause token.
+	 * @return The address of the fundraiser.
+	 */
+	function getFundraiser(uint256 tokenId) public view returns (address) {
+		return causes[tokenId].fundraiser;
+	}
 
-    /**
-     * @notice Grant the minter role to an address.
-     * @param minter The address to be granted the minter role.
-     */
-    function grantMinterRole(address minter) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(MINTER_ROLE, minter);
-    }
+	/**
+	 * @notice Grant the minter role to an address.
+	 * @param minter The address to be granted the minter role.
+	 */
+	function grantMinterRole(
+		address minter
+	) public onlyRole(DEFAULT_ADMIN_ROLE) {
+		_grantRole(MINTER_ROLE, minter);
+	}
 
-    /**
-     * @notice Grant the Givvest role to a contract address.
-     * @param givvestContract The contract address to be granted the GIVVEST_ROLE.
-     */
-    function grantGivvestRole(address givvestContract) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(GIVVEST_ROLE, givvestContract);
-    }
+	/**
+	 * @notice Grant the Givvest role to a contract address.
+	 * @param givvestContract The contract address to be granted the GIVVEST_ROLE.
+	 */
+	function grantGivvestRole(
+		address givvestContract
+	) public onlyRole(DEFAULT_ADMIN_ROLE) {
+		_grantRole(GIVVEST_ROLE, givvestContract);
+	}
 
-    /**
-     * @notice Change the ownership of the contract to a new address.
-     * @param to The address of the new owner.
-     */
-    function changeOwnership(address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(DEFAULT_ADMIN_ROLE, to);
-        _revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
+	/**
+	 * @notice Change the ownership of the contract to a new address.
+	 * @param to The address of the new owner.
+	 */
+	function changeOwnership(address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
+		_grantRole(DEFAULT_ADMIN_ROLE, to);
+		_revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
-        emit OwnershipChanged(to);
-    }
+		emit OwnershipChanged(to);
+	}
 
-    /**
-     * @dev Override for `_update` function to ensure compatibility with multiple inheritance.
-     */
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
+	/**
+	 * @dev Override for `_update` function to ensure compatibility with multiple inheritance.
+	 */
+	function _update(
+		address to,
+		uint256 tokenId,
+		address auth
+	) internal override(ERC721, ERC721Enumerable) returns (address) {
+		return super._update(to, tokenId, auth);
+	}
 
-    /**
-     * @dev Override for `_increaseBalance` function to ensure compatibility with multiple inheritance.
-     */
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
+	/**
+	 * @dev Override for `_increaseBalance` function to ensure compatibility with multiple inheritance.
+	 */
+	function _increaseBalance(
+		address account,
+		uint128 value
+	) internal override(ERC721, ERC721Enumerable) {
+		super._increaseBalance(account, value);
+	}
 
-    /**
-     * @notice Get the token URI for a specific token ID.
-     * @param tokenId The ID of the token.
-     * @return The token URI.
-     */
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+	/**
+	 * @notice Get the token URI for a specific token ID.
+	 * @param tokenId The ID of the token.
+	 * @return The token URI.
+	 */
+	function tokenURI(
+		uint256 tokenId
+	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+		return super.tokenURI(tokenId);
+	}
 
-    /**
-     * @dev Override for `supportsInterface` function to ensure compatibility with multiple inheritance.
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+	/**
+	 * @dev Override for `supportsInterface` function to ensure compatibility with multiple inheritance.
+	 */
+	function supportsInterface(
+		bytes4 interfaceId
+	)
+		public
+		view
+		override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl)
+		returns (bool)
+	{
+		return super.supportsInterface(interfaceId);
+	}
 }
